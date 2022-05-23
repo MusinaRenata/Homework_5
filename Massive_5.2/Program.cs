0130dae065ffa9bf1237a861c3de6437124fb00b
Renata <myska2@yandex.ru>
2022-05-23 15:23:07 +0300

    t5.2

---
+++ Massive_5.2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 2. Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = new int[5];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 15);
+    Console.Write(array[i] + " ");
+
+    if (i % 2 > 0)
+    {
+        sum = sum + array[i];
+    }
+}
+
+Console.WriteLine(" ");
+Console.WriteLine(sum);
